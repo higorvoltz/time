ca3a7606c93c763e042c31798f2a289814516a7d
higorvoltz <higor.amorim@voltzmotors.com>
2022-12-12 08:55:29 -0300

    add gem device with profile and admin passwords and gem I18n

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  http_basic_authenticate_with name: 'higor', password: 'amorim'
   def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
   get 'welcome/index'
 
   root to: 'welcome#index'
