c2913ebd6e1229fe80f5e0582e2732db0f35378f
higorvoltz <higor.amorim@voltzmotors.com>
2022-12-13 09:21:49 -0300

    add admin and profile backoffice index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :profile_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admin_backoffice do
+    get 'welcome/index'
+  end
   devise_for :profiles
   devise_for :admins
   get 'welcome/index'
