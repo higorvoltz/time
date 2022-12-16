8a3d25b850b2bbfb8e9c63f349ccb3fd19794457
higorvoltz <higor.amorim@voltzmotors.com>
2022-12-16 08:32:25 -0300

    fixing route from profiles to users in controller

--- app/controllers/profile_backoffice/welcome_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module ProfileBackoffice
-  # welcome message
-  class WelcomeController < ProflesBackofficeController
-    def index; end
-  end
-end
--- test/controllers/profile_backoffice/welcome_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ProfileBackoffice::WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get profile_backoffice_welcome_index_url
-    assert_response :success
-  end
-end
